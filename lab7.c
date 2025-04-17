7a47cc32a9f9105510ebf605ef17e0aeb7eb5c6d
afeefa-github <afeefa.23cs006@sode-edu.in>
2025-04-17 14:15:33 +0530

    Create lab7.c

---
+++ lab7.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#define MAX_ITEMS 10
+int knapsack(int w,int wt[],int val[],int n){
+int i,j;
+int v[MAX_ITEMS+1][w+1];
+for(i=0;i<=n;i++){
+for(j=0;j<=w;j++){
+if(i==0||j==0){
+v[i][j]=0;
+}else if(j-wt[i]<0){
+v[i][j]=v[i-1][j];
+}
+else{
+v[i][j]=max(v[i-1][j],val[i]+v[i-1][j-wt[i]]);
+}}}
+return v[n][w];
+}
+int max(int a,int b)
+{
+return(a>b)?a:b;
+}
+void main(){
+int i,n,w,wt[MAX_ITEMS],val[MAX_ITEMS];
+printf("enter the number of items:");
+scanf("%d",&n);
+printf("enter the max capacity:");
+scanf("%d",&w);
+printf("enter the weight:");
+for(i=1;i<=n;i++)
+{
+scanf("%d",&wt[i]);
+}
+printf("enter the profit values\n:");
+for(i=1;i<=n;i++)
+{
+scanf("%d",&val[i]);
+}
+printf("the maximum profit is %d\n",knapsack(w,wt,val,n));
+}
