908a1debaf9bc09e057aeb6c0a31981c6d227959
afeefa-github <afeefa.23cs006@sode-edu.in>
2025-05-15 15:37:41 +0530

    Add files via upload

---
+++ subset.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define MAX 20
+int x[MAX];
+int s[MAX];
+int d,flag=0;
+void sumofsub(int m,int k,int r)
+{
+int i;
+x[k]=1;
+if(m+s[k]==d)
+{
+flag=1;
+printf("{");
+for(i=1;i<=k;i++)
+{
+if(x[i]==1)
+{
+printf("%d",s[i]);
+}
+}
+printf("}\n");
+}
+else if((m+s[k]+s[k+1])<=d)
+{
+sumofsub(m+s[k],k+1,r-s[k]);
+}
+if((m+r-s[k]>=d)&&(m+s[k+1]<=d))
+{
+x[k]=0;
+sumofsub(m,k+1,r-s[k]);
+}
+}
+int main()
+{
+int n,i,sum=0;
+printf("enter the number of elements:");
+scanf("%d",&n);
+printf("enter the elements:\n");
+for( i=1;i<=n;i++)
+{
+scanf("%d",&s[i]);
+}
+printf("enter the value of d:");
+scanf("%d",&d);
+for( i=1;i<=n;i++)
+{
+sum+=s[i];
+}
+if(sum<d||s[1]>d)
+{
+printf("the given problem instance does not have a solution\n");
+return 0;
+}
+else
+{
+printf("subsets are:\n");
+sumofsub(0,1,sum);
+}
+if(flag==0)
+{
+printf("no subset possible\n");
+}
+return 0;
+}
